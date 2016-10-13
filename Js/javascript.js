0352f774ecd2d155972845db1928605297a458c2
Michael Gaudet <Michael.G224@gmail.com>
2016-10-12 18:12:15 -0700

    found a better way to accordion via jQuery

--- Js/javascript.js
+++ Js/javascript.js
@@ -1,11 +1,14 @@
 
 $(document).ready(function(){
-	
-	$('#info').accordion({
-		collapsible: true,
-		animate: {
-        duration: 0.00
-   		}	
+	$('#about').on('click',function(){
+		if($('#about').hasClass('active')){
+			$('.about-info').slideUp();
+			$('#about').removeClass('active');
+		}
+		else{
+			$('#about').addClass('active');
+			$('.about-info').slideDown();
+		}
 	});
-
+	
 });
