3ab5d79856b422ee2ac263c390fb8ef528e70657
Michael Gaudet <Michael.G224@gmail.com>
2016-10-12 17:15:19 -0700

    fixxed accordion and started about-me

--- Js/javascript.js
+++ Js/javascript.js
@@ -1,8 +1,11 @@
-var about = '<p class="well">My name is michael Gaudet. I am a self taught programmer from Sonora California.</p>';
 
 $(document).ready(function(){
 	
 	$('#info').accordion({
-		collapsible: true	});
+		collapsible: true,
+		animate: {
+        duration: 0.00
+   		}	
+	});
 
 });
