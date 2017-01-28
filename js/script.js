95d278eeae36e1d1992e5e3ee00daba3340a704e
Michael Gaudet <Michael.G224@gmail.com>
2017-01-28 10:49:17 -0600

    added functionality to the top buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
 
+$(document).ready(function(){
+    $('.projects').hide();
+    $('.contact').hide();
+    $('.about-toggle').on('click', function(){
+        $('.about').slideToggle();
+        $('.projects').slideUp();
+        $('.contact').slideUp();
+    });
+    $('.contact-toggle').on('click', function(){
+        $('.contact').slideToggle();
+        $('.projects').slideUp();
+        $('.about').slideUp();
+    });
+    $('.project-toggle').on('click', function(){
+        $('.projects').slideToggle();
+        $('.about').slideUp();
+        $('.contact').slideUp();
+    });
 });
