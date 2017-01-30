f00acc26d024efdd55de518d58a87586a71186ac
Michael Gaudet <Michael.G224@gmail.com>
2017-01-30 13:48:34 -0600

    changed around some of the text in the html and added a delay to the slideToggle

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ $(document).ready(function(){
     $('.projects').hide();
     $('.contact').hide();
     $('.about-toggle').on('click', function(){
-        $('.about').slideToggle();
+        $('.about').delay(800).slideToggle();
         $('.projects').slideUp();
         $('.contact').slideUp();
     });
     $('.contact-toggle').on('click', function(){
-        $('.contact').slideToggle();
+        $('.contact').delay(400).slideToggle();
         $('.projects').slideUp();
         $('.about').slideUp();
     });
     $('.project-toggle').on('click', function(){
-        $('.projects').slideToggle();
+        $('.projects').delay(800).slideToggle();
         $('.about').slideUp();
         $('.contact').slideUp();
     });
