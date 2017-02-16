818da5193219236a92d1d2fa55ec485fa9a0e3bc
Michael Gaudet <Michael.G224@gmail.com>
2017-02-15 18:16:46 -0600

    debugging

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.info').hide(0)
   $('.navDD').on('click',function(){
-    $('.ddNavs').slideToggle();
+    $('.ddNavs').slideToggle(800);
     if($('.navDD').hasClass('down')){
       $(".navDD").removeClass('down')
       $(".navDD").html("<i class='fa fa-caret-up'></i>")
@@ -13,10 +13,10 @@ $(document).ready(function(){
   $('.contact-button').on('click', function(){
     if($('.info').hasClass('hidden')){
       $('.info').removeClass('hidden')
-      $('.info').show(1000);
+      $('.info').show(800);
     }else{
       $('.info').addClass('hidden')
-      $('.info').hide(1000);
+      $('.info').hide(800);
     }
   })
 
