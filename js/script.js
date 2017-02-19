788bff72c4b4cb070386d2c65625220f72c5f85b
Michael Gaudet <Michael.G224@gmail.com>
2017-02-18 23:06:25 -0600

    Getting close to perfect

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
-$(document).ready(function(){
-  console.log('ready');
-  $('.normal-navs-scroll').hide();
-  $(window).scroll(function(){
-    if($(window).scrollTop()>10){
-      $('.normal-navs').slideUp(500)
-      $('.normal-navs-scroll').delay(550).slideDown(500);
-      console.log(true);
-    }else{
-
-      console.log(false);
-    }
-  })
+$(document).ready(function() {
+    console.log('ready');
+    $('.normal-navs-scroll').hide();
+    $(window).scroll(function() {
+        //controls navbar background toggle
+        if ($(window).scrollTop() > 10) {
+            $('.normal-navs').slideUp(500)
+            $('.normal-navs-scroll').slideDown(500);
+        } else {
+            $('.normal-navs-scroll').slideUp()
+            $('.normal-navs').slideDown()
+        }
+        if($('.text').isOnScreen(function(deltas){
+  return deltas.top >= this.height() && deltas.bottom >= this.height();
+})){
+          $('.normal-home').removeClass("highlighted")
+          $('.normal-about').addClass('highlighted')
+        }else{
+          $('.normal-home').addClass("highlighted")
+          $('.normal-about').removeClass('highlighted')
+        }
+    })
+    $(".normal-about").click(function() {
+        $('html,body').animate({
+                scrollTop: $(".aboutlink").offset().top
+            },
+            'slow');
+    });
+    $(".normal-home").click(function() {
+        $('html,body').animate({
+                scrollTop: $(".homelink").offset().top
+            },
+            'slow');
+    });
 })
