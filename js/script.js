83617e50dbe1cdb88ddc60598a22e498d9f8619b
Michael Gaudet <Michael.G224@gmail.com>
2017-02-18 19:52:05 -0600

    added mod to lander text

--- js/script.js
+++ js/script.js
@@ -1,32 +1,3 @@
 $(document).ready(function(){
-  $('.info').hide(0)
-  $('.navDD').on('click',function(){
-    $('.ddNavs').slideToggle(800);
-    if($('.navDD').hasClass('down')){
-      $(".navDD").removeClass('down')
-      $(".navDD").html("<i class='fa fa-caret-up'></i>")
-    }else{
-      $(".navDD").addClass('down')
-      $(".navDD").html("<i class='fa fa-caret-down'></i>")
-    }
-  })
-  $('.contact-button').on('click', function(){
-    if($('.info').hasClass('hidden')){
-      $('.info').removeClass('hidden')
-      $('.info').show(800);
-    }else{
-      $('.info').addClass('hidden')
-      $('.info').hide(800);
-    }
-  })
-
-  $(window).scroll(function(){
-    if($(window).scrollTop()>10){
-      $('.navContainer').addClass('navContainer-scroll')
-    }
-    else{
-      $('.navContainer').removeClass('navContainer-scroll')
-    }
-  })
-
+  $('#fullpage').fullpage();
 })
