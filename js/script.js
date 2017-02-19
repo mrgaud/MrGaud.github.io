ca34d5df19a042dce3f96650dcd31d6090a0a3ae
Michael Gaudet <Michael.G224@gmail.com>
2017-02-18 21:32:57 -0600

    remodel overhaul on navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-  $('#fullpage').fullpage();
+  console.log('ready');
+  $('.normal-navs-scroll').hide();
+  $(window).scroll(function(){
+    if($(window).scrollTop()>10){
+      $('.normal-navs').slideUp(500)
+      $('.normal-navs-scroll').delay(550).slideDown(500);
+      console.log(true);
+    }else{
+
+      console.log(false);
+    }
+  })
 })
